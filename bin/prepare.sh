1027ea114df16046f19b791b20c5ca1d84cfd209
miya0001 <miya@wpist.me>
2017-09-17 23:53:24 +0900

    some fix

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -7,12 +7,12 @@ echo "PHP Version: ${PHP_VERSION}"
 docker pull miya0001/phpenv:$PHP_VERSION
 docker run -idt --name phpenv --privileged --volume="$(pwd)":/shared/:rw miya0001/phpenv:$PHP_VERSION "/bin/bash"
 
-docker exec phpenv ln -s /var/run/mysqld/mysqld.sock /tmp/mysql.sock || echo "/tmp/mysql.sock already exists."
-
 # Init MySQL
 docker exec phpenv sudo chown -R mysql:mysql /var/lib/mysql
 docker exec phpenv sudo mysql_install_db --datadir=/var/lib/mysql --user=mysql
 docker exec phpenv sudo service mysql start
+
+docker exec phpenv ln -s /var/run/mysqld/mysqld.sock /tmp/mysql.sock || echo "/tmp/mysql.sock already exists."
 
 # Creates MySQL database for WordPress
 docker exec phpenv mysql -u root -e "CREATE DATABASE wordpress_tests;" -p1111
